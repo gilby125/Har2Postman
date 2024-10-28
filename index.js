b41de1d55f4b69cdc521a9d92ede19f179770adf
gilby125 <gilby1@gmail.com>
2024-10-28 11:52:37 -0500

    docker

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const express = require('express');
 const HarToPostman = require('./lib/har-to-postman.js');
+const app = express();
+const port = 3003;
+
+app.use(express.json());
+
+app.post('/convert', (req, res) => {
+  const harContent = req.body.harContent;
+  const includeTest = req.body.includeTest || false;
+  const result = HarToPostman.createPostmanCollection(harContent, includeTest);
+  res.json({ postmanCollection: result });
+});
+
+app.listen(port, () => {
+  console.log(`Har2Postman service running on port ${port}`);
+});
 
 module.exports = HarToPostman;
